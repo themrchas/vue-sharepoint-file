9a198d24a9d14febc47cd0a24423b0040fb878f8
Chas <chas@mail>
2025-04-10 12:20:22 +0000

    basic vuetify component integrated

--- src/main.js
+++ src/main.js
@@ -1,16 +1,32 @@
 import { defineCustomElement } from 'vue'
 
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components'
+
+//import * as components from 'vuetify/components'
+import { VApp, VContainer, VFileInput } from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+
 import 'vuetify/styles'
 
 import  Test  from './components/TestElement.ce.vue'
 
 const vuetify = createVuetify({
+    components : {
+        VApp, VContainer, VFileInput
+    },
+    directives,
+  })
+
+/*
+const vuetify = createVuetify({
     components,
     directives,
   })
+    */
+
+//const element = defineCustomElement(Test);
+
 
 const element = defineCustomElement(Test, {
     configureApp(app) {
